0219ec9bca13eedf9e6bcbeeb715cc451b06e35b
Trouni Tiet <trouni@gmail.com>
2019-07-10 10:38:01 +0900

    added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/index'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
+  get 'rooms/index'
+  get 'rooms/show'
+  get 'battles/show'
   devise_for :users
   root to: 'pages#home'
   resources :rooms
-  resources :battles, only: :show
+  resources :battles, only: :index
 end
