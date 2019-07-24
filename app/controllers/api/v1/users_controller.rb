af24fb96840fc8780249f40bca37babe05b37d09
Trouni Tiet <trouni@gmail.com>
2019-07-25 01:07:29 +0900

    prevent useless fetching of data

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,7 +7,14 @@ class Api::V1::UsersController < ApplicationController
 
   def fetch_data
     skip_authorization
-    FetchCompletedChallengesJob.perform_now(params[:id] || params[:user_id], params[:battle_id])
-    render json: { status: "success" }
+
+    if Battle.find(params[:battle_id]).survived?(User.find(params[:user_id]))
+      render json: { status: "challenge already completed" }
+    elsif User.find(params[:user_id]).last_fetched_at > (DateTime.now - 5.seconds)
+      render json: { status: "job already scheduled less than 5 seconds ago" }
+    else
+      FetchCompletedChallengesJob.perform_later(params[:id] || params[:user_id], params[:battle_id])
+      render json: { status: "fetching job scheduled" }
+    end
   end
 end
