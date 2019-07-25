57c8a6b839284f4d468545bcce5d4602213c23fe
Trouni Tiet <trouni@gmail.com>
2019-07-25 15:03:31 +0900

    config action cable for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,8 +45,10 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
+  # config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
+  config.web_socket_server_url = "wss://#{ENV['RAILS_HOST']}/cable"
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = ['https://#{ENV['RAILS_HOST']}', 'http://#{ENV['RAILS_HOST']}']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
