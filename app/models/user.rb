b131fec20445e4bf9b99a93dc8063f0d0148686b
Trouni Tiet <trouni@gmail.com>
2019-07-26 14:19:53 +0900

    fixed email signup issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,6 +38,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  before_create :fill_email
   after_create :async_fetch_codewars_info
 
   def email_required?
@@ -139,5 +140,9 @@ class User < ApplicationRecord
       "room_#{room.id}",
       api_expose.merge(unsubscribed: true)
     )
+  end
+
+  def fill_email
+    email = "#{username}@me.com" unless email
   end
 end
