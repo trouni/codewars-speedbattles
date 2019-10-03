16246e88fd191eba0cd52597e3f478deff9e9a28
Trouni Tiet <trouni@gmail.com>
2019-10-03 10:37:13 +0900

    redis heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
-  config.action_cable.url = "wss://www.yourdomain.top/cable"
+  config.action_cable.url = "wss://#{ENV['RAILS_HOST']}/cable"
   # config.web_socket_server_url = "wss://#{ENV['RAILS_HOST']}/cable"
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
   config.action_cable.allowed_request_origins = ["https://#{ENV['RAILS_HOST']}", "http://#{ENV['RAILS_HOST']}"]
--- config/routes.rb
+++ config/routes.rb
@@ -13,28 +13,28 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
 
-  # API Routes
-  namespace :api, defaults: { format: :json } do
-    # mount ActionCable.server => '/cable'
-    namespace :v1 do
-      resources :rooms, only: [ :index, :show, :create, :update ] do
-        get '/leaderboard', to: 'rooms#leaderboard'
-        resources :battles, only: :create
-        resources :users, only: :index
-      end
-      resources :battles, only: %i[show update destroy] do
-        get '/invitation', to: 'battles#invitation'
-        get '/launch', to: 'battles#launch'
-        # get '/results', to: 'battles#results'
-        # get '/completed_battle', to: 'battles#completed_battle'
-      end
-      resources :users do
-        get '/fetch_data', to: 'users#fetch_data'
-      end
-      get '/check_username', to: 'users#valid_username?'
-      resources :challenges, only: :show
-      resources :chats, only: :show
-      resources :messages, only: :create
-    end
-  end
+  # # API Routes
+  # namespace :api, defaults: { format: :json } do
+  #   # mount ActionCable.server => '/cable'
+  #   namespace :v1 do
+  #     resources :rooms, only: [ :index, :show, :create, :update ] do
+  #       get '/leaderboard', to: 'rooms#leaderboard'
+  #       resources :battles, only: :create
+  #       resources :users, only: :index
+  #     end
+  #     resources :battles, only: %i[show update destroy] do
+  #       get '/invitation', to: 'battles#invitation'
+  #       get '/launch', to: 'battles#launch'
+  #       # get '/results', to: 'battles#results'
+  #       # get '/completed_battle', to: 'battles#completed_battle'
+  #     end
+  #     resources :users do
+  #       get '/fetch_data', to: 'users#fetch_data'
+  #     end
+  #     get '/check_username', to: 'users#valid_username?'
+  #     resources :challenges, only: :show
+  #     resources :chats, only: :show
+  #     resources :messages, only: :create
+  #   end
+  # end
 end
