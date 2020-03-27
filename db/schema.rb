4e8382e406d009d52d716dd3735e7f9ccf5ef4c1
Trouni Tiet <trouni@gmail.com>
2020-03-27 18:19:06 +0900

    added sound attribute to rooms

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_21_043503) do
+ActiveRecord::Schema.define(version: 2020_03_27_091715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2019_10_21_043503) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "show_stats", default: true
+    t.string "sound", default: "everyone"
     t.index ["moderator_id"], name: "index_rooms_on_moderator_id"
   end
 
