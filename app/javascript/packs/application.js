52b1b2a7d8c2b0019086ddc656732bdf7d18bff4
Trouni Tiet <trouni@gmail.com>
2020-03-29 02:02:07 +0900

    added clickoutside directive

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,6 +23,22 @@ Vue.use(VueShowdown, {
   },
 })
 
+Vue.directive('click-outside', {
+  bind: function (el, binding, vnode) {
+    el.clickOutsideEvent = function (event) {
+      // here I check that click was outside the el and his childrens
+      if (!(el == event.target || el.contains(event.target))) {
+        // and if it did, call method provided in attribute value
+        vnode.context[binding.expression](event);
+      }
+    };
+    document.body.addEventListener('click', el.clickOutsideEvent)
+  },
+  unbind: function (el) {
+    document.body.removeEventListener('click', el.clickOutsideEvent)
+  },
+});
+
 Vue.use(ActionCableVue, {
   debug: process.env.NODE_ENV === "development",
   debugLevel: 'error',
