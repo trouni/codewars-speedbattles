d76d6fcaab86085cd3fff33f93fd169bd2622602
Trouni Tiet <trouni@gmail.com>
2020-03-29 01:59:11 +0900

    updated navbar design with std-button

--- app/javascript/components/navbar.vue
+++ app/javascript/components/navbar.vue
@@ -1,15 +1,24 @@
 <template>
-  <div id="menu-button">
+  <div id="menu-button" v-click-outside="hideMenu">
     <div :class="['dropdown-toggle highlight-on-hover', { active: showMenu }]" @click="showMenu = !showMenu">
       <i class="fab fa-2x fa-old-republic"></i>
     </div>
-    <div v-if="showMenu" :class="['menu widget-bg h-auto']">
+    <div v-if="showMenu" :class="['menu widget-bg h-auto p-0']">
       <div class="widget">
         <!-- <h3 class="header">SpeedBattles</h3> -->
-        <div class="widget-body">
-          <a v-if="roomId >= 0" href="/rooms/" @click="showMenu = false" class="button mx-3"><i class="fas fa-angle-double-left"></i>Leave room</a>
-          <a @click="showMenu = false" :href="[roomId >= 0 ? `/users/edit/?current_room_id=${roomId}` : '/users/edit/']" class="button mx-3"><i class="fas fa-cog"></i>Settings</a>
-          <a href="/users/sign_out" data-method="delete" class="button mx-3" @click="showMenu = false"><i class="fas fa-sign-out-alt"></i>Log out</a>
+        <div class="widget-body py-3">
+          
+          <a href="/users/sign_out" data-method="delete" class="mx-3" @click="hideMenu">
+            <std-button fa-icon="fas fa-sign-out-alt" title="Log out" />
+          </a>
+          <a v-if="roomId >= 0" href="/rooms/" @click="hideMenu" class="mx-3">
+            <std-button fa-icon="fas fa-angle-double-left" title="Leave room" />
+          </a>
+          <a @click="hideMenu" :href="[roomId >= 0 ? `/users/edit/?current_room_id=${roomId}` : '/users/edit/']" class="mx-3">
+            <std-button fa-icon="fas fa-cog" title="Settings" />
+          </a>
+          <std-button v-if="showSoundControls" @click.native="$root.$emit('toggle-music')" :class="['mx-3', { disabled: !sounds.musicOn }]" fa-icon="fas fa-music" title="Music" />
+          <std-button v-if="showSoundControls" @click.native="$root.$emit('toggle-sound-fx')" :class="['mx-3', { disabled: !sounds.soundFxOn }]" fa-icon="fas fa-drum" title="SFX" />
         </div>
       </div>
     </div>
@@ -20,6 +29,11 @@
   export default {
     props: {
       roomId: Number,
+      sounds: Object,
+      showSoundControls: Boolean,
+    },
+    components: {
+      StdButton: () => import('./shared/button.vue'),
     },
     data() {
       return {
@@ -29,12 +43,16 @@
     // computed: {
     // },
     methods: {
+      hideMenu() {
+        this.showMenu = false;
+      }
     }
   }
 </script>
 
 <style scoped>
 .widget {
-  background: linear-gradient(154.9deg, rgba(0, 0, 0, 0.6) 0%, rgba(0, 0, 0, 0.9) 86.37%);
+  min-height: unset;
+  background: linear-gradient(154.9deg, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 1) 86.37%);
 }
 </style>
