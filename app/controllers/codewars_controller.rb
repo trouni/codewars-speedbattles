637b948981aca5ffed2f2b8355a33082278b5258
Trouni Tiet <trouni@gmail.com>
2020-04-18 22:53:45 +0900

    fixing wrong rank key

--- app/controllers/codewars_controller.rb
+++ app/controllers/codewars_controller.rb
@@ -22,11 +22,12 @@ class CodewarsController < ApplicationController
     codewars_id = user[:id]
     user = User.find_by(codewars_id: codewars_id) || fetch_user(id: codewars_id)
     if user
-      user.update(codewars_id: codewars_id)
+      user.assign_attributes(codewars_id: codewars_id)
       codewars_honor = user[:honor]
-      user.update(codewars_honor: codewars_honor) if codewars_honor
+      user.assign_attributes(codewars_honor: codewars_honor) if codewars_honor
       codewars_rank = user[:rank]
-      user.update(codewars_rank: codewars_rank) if codewars_rank
+      user.assign_attributes(codewars_overall_rank: codewars_rank) if codewars_rank
+      user.save
       FetchCompletedChallengesJob.perform_later(user_id: user.id, all_pages: false)
     end
   end
