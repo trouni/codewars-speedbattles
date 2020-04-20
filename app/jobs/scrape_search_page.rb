7d7d6a9384961fadce65e1ad3bdba9bb56f981fa
Trouni Tiet <trouni@gmail.com>
2020-04-20 15:19:13 +0900

    fixed language value for scraping pages

--- app/jobs/scrape_search_page.rb
+++ app/jobs/scrape_search_page.rb
@@ -5,6 +5,6 @@ class ScrapeSearchPage < ApplicationJob
   def perform(language: 'ruby', page_number: 0, last_page: 0)
     puts "Scraping search results page #{page_number} of #{last_page} for all #{language} katas..."
     scrape_search_page(page_number: page_number)
-    ScrapeSearchPage.set(wait: 10.seconds).perform_later(page_number: page_number + 1, last_page: last_page) if page_number < last_page
+    ScrapeSearchPage.set(wait: 10.seconds).perform_later(language: language, page_number: page_number + 1, last_page: last_page) if page_number < last_page
   end
 end
