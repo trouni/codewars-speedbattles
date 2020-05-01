2b038781f20c81df6281b2db5c91e7f043cfa5a8
Trouni Tiet <trouni@gmail.com>
2020-05-01 12:15:06 +0900

    protected against nil

--- app/jobs/fetch_completed_challenges_job.rb
+++ app/jobs/fetch_completed_challenges_job.rb
@@ -5,7 +5,7 @@ class FetchCompletedChallengesJob < ApplicationJob
   # user_id, battle_id, all_pages
   def perform(user_id:, all_pages: true)
     @user = User.find(user_id)
-    if @user.last_fetched_at > Time.now - 1.minute
+    if @user.last_fetched_at && @user.last_fetched_at > Time.now - 1.minute
       puts 'Fetched less than 1 min ago. Skipping fetch.'
       return
     end
@@ -18,7 +18,7 @@ class FetchCompletedChallengesJob < ApplicationJob
       (1...total_pages).each { |page| fetch_page(@user, page) } if all_pages
     end
     announce_completion if @battle && @user.survived?(@battle) && !already_completed_battle
-    @user.room&.broadcast_player(user: @user) if @battle.players.include?(@user)
+    @user.room&.broadcast_player(user: @user) if @battle&.players&.include?(@user)
   end
 
   private
