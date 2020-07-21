7a3c676eaf7bd579f8cbf489bb7391c40cd655d2
Trouni Tiet <trouni@gmail.com>
2020-07-21 15:13:18 +0900

    end battle at time from time_limit

--- app/jobs/end_battle.rb
+++ app/jobs/end_battle.rb
@@ -1,11 +1,13 @@
 class EndBattle < ApplicationJob
   queue_as :critical
 
-  def perform(battle_id:, force: false)
+  def perform(battle_id:, end_at: nil, force: false)
     battle = Battle.find(battle_id)
     return unless latest_job_for?(battle.room) || force
 
-    battle.terminate
+    expected_end_time = battle&.start_time + battle&.time_limit.seconds
+    end_at ||= expected_end_time if expected_end_time < Time.now
+    battle.terminate(end_at: end_at)
     # battle.room.clear_next_event!(only: 'end-battle')
   end
 end
