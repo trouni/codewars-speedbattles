291e85405036ac6c61768f4d28f8528043b83904
Trouni Tiet <trouni@gmail.com>
2020-07-22 12:18:57 +0900

    execute sql method

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,13 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def execute_sql(sql, args = {})
+    results = ActiveRecord::Base.connection.execute(sql, args)
+  
+    if results.present?
+      return results
+    else
+      return nil
+    end
+  end
 end
