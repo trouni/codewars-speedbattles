88cdee44fa4fdb9919906e677668c18b8cf1d072
Trouni Tiet <trouni@gmail.com>
2020-07-23 19:26:03 +0900

    removed commented code

--- app/models/room.rb
+++ app/models/room.rb
@@ -252,11 +252,6 @@ class Room < ApplicationRecord
     set_next_event(at: Time.now + delay, type: type, jid: jid)
   end
 
-  # def schedule_next_battle(delay = 60.seconds)
-  #   set_timer(delay, 'next-battle')
-  #   CreateRandomBattle.set(wait: delay - 1.second).perform_later(room_id: id)
-  # end
-
   private
 
   def create_chat
@@ -266,84 +261,4 @@ class Room < ApplicationRecord
   def announce_new_name
     announce(:chat, "War room renamed to <strong>#{name}</strong>.")
   end
-
-
-  # def clear_next_event!(only: nil)
-  #   set_next_event(at: Time.now) if only.nil? || only == next_event[:type]
-  # end
-
-  # def players
-  #   super.distinct
-  #   # User.joins(battle_invites: :battle).where(battle_invites: { confirmed: true }, battles: { room_id: id }).uniq
-  # end
-
-  # def leaderboard
-  #   query_fought = <<-SQL
-  #   SELECT COUNT(*) AS battles_fought, u.id AS id, u.username AS username, u.name AS name, u.codewars_honor, u.codewars_clan, u.codewars_leaderboard_position, u.codewars_overall_rank, u.codewars_overall_score
-  #   FROM battles b
-  #   JOIN battle_invites bi ON b.id = bi.battle_id
-  #   JOIN users u ON bi.player_id = u.id
-  #   WHERE bi.confirmed = true
-  #   AND b.room_id = #{id}
-  #   AND b.end_time IS NOT NULL
-  #   GROUP BY u.id
-  #   SQL
-
-  #   query_survived = <<-SQL
-  #   SELECT COUNT(*) AS battles_survived, u.id AS id
-  #   FROM battles b
-  #   JOIN battle_invites bi ON b.id = bi.battle_id
-  #   JOIN users u ON bi.player_id = u.id
-  #   LEFT JOIN completed_challenges cc ON cc.user_id = u.id
-  #   WHERE bi.confirmed = true
-  #   AND b.room_id = #{id}
-  #   AND cc.completed_at > b.start_time
-  #   AND cc.completed_at < b.end_time
-  #   AND cc.kata_id = b.kata_id
-  #   GROUP BY u.id
-  #   SQL
-
-  #   query = <<-SQL
-  #   SELECT FirstQuery.id, FirstQuery.username, FirstQuery.name, FirstQuery.codewars_honor, FirstQuery.battles_fought AS battles_fought, COALESCE(SecondQuery.battles_survived, 0) AS battles_survived, (COALESCE(FirstQuery.battles_fought, 0) - COALESCE(SecondQuery.battles_survived, 0)) AS battles_lost, (COALESCE(SecondQuery.battles_survived, 0) * 5) - (COALESCE(FirstQuery.battles_fought, 0) - COALESCE(SecondQuery.battles_survived, 0)) AS total_score
-  #   FROM (#{query_fought}) AS FirstQuery
-  #   LEFT JOIN (#{query_survived}) AS SecondQuery ON FirstQuery.id = SecondQuery.id
-  #   ORDER BY total_score DESC
-  #   SQL
-
-  #   leaderboard = {}
-  #   ActiveRecord::Base.connection.exec_query(query).as_json.each { |e| leaderboard[e["id"].to_s] = e }
-  #   return leaderboard
-  #   # return ActiveRecord::Base.connection.exec_query(query).as_json
-  # end
-
-  # def battles_fought(player)
-  #   BattleInvite.includes(:player, :battle, :room).joins(:player, :battle, :room).where(
-  #     confirmed: true,
-  #     battles: { room_id: id },
-  #     users: { id: player }
-  #   )
-  # end
-
-  # def battles_survived(player)
-  #   # Battle.joins(battle_invites: { player: :completed_challenges }).where(
-  #   BattleInvite.includes(:player, :room, player: :completed_challenges).joins(:player, :room, player: :completed_challenges).where(
-  #     "battle_invites.confirmed = true AND battles.room_id = ? AND completed_challenges.completed_at > battles.start_time AND completed_challenges.completed_at < battles.end_time AND completed_challenges.kata_id = battles.kata_id AND completed_challenges.user_id = ?",
-  #     id,
-  #     player.id
-  #   )
-  # end
-
-  # def total_score(player)
-  #   finished_battles.map { |battle| battle.score(player) }.reduce(:+)
-  # end
-
-  # def old_players_info
-  #   players.includes(:battles, :room, :completed_challenges, :battle_invites)
-  #                   .map { |user| user.unused_api_expose(self, active_battle) }
-  # end
-
-  # def old_users_info
-  #   users.includes(:battles, :room, :completed_challenges, :battle_invites)
-  #                   .map { |user| user.unused_api_expose(self, active_battle) }
-  # end
 end
