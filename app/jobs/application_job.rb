3b863906588486d25bc394c3638e1b774955e3e2
Trouni Tiet <trouni@gmail.com>
2020-07-31 22:01:24 +0900

    fixed the default scope and condition on first login

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,9 +1,10 @@
 class ApplicationJob < ActiveJob::Base
   private
 
-  def latest_job_for?(resource, scope: "default")
-    next_jid = scope ? resource.settings(:base).next_jid[scope] : resource.settings(:base).next_jid
-    result = next_jid == job_id
+  def latest_job_for?(resource, scope: nil)
+    scope ||= 'default'
+    next_jid = resource.settings(:base).next_jid[scope]
+    result = next_jid.nil? || next_jid == job_id
     puts "SKIPPING: Job #{job_id} for #{resource.class} ##{resource.id} has expired." unless result
     result
   end
