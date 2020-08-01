a6a9dbb3f1f1f6b8cd9b62aeed7d5ab19192739f
Trouni Tiet <trouni@gmail.com>
2020-08-01 20:11:26 +0900

    vue layout for home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    render layout: 'vue_application'
   end
 
   def webhook_help
