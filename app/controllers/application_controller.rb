70f5c5986ccce6594ef78bf7f34ee280418a7165
Trouni Tiet <trouni@gmail.com>
2020-08-03 01:56:30 +0900

    fixed redirect after login from home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :store_action, if: :storable_location?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  after_action :store_action
   include Pundit
 
   # Pundit: white-list approach.
@@ -30,16 +30,16 @@ class ApplicationController < ActionController::Base
     devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
   
+  def storable_location?
+    request.get? &&
+    # Not storing the home page
+    request.path != "/" &&
+    is_navigational_format? &&
+    !devise_controller? &&
+    !request.xhr?
+  end
+
   def store_action
-    return unless request.get? 
-    if (request.path != "/users/sign_in" &&
-        request.path != "/users/sign_up" &&
-        request.path != "/users/password/new" &&
-        request.path != "/users/password/edit" &&
-        request.path != "/users/confirmation" &&
-        request.path != "/users/sign_out" &&
-        !request.xhr?) # don't store ajax calls
-      store_location_for(:user, request.fullpath)
-    end
+    store_location_for(:user, request.fullpath)
   end
 end
