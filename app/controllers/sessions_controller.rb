8254ea4b478cacb356ca774ed2ef6ffed4bb5a59
Trouni Tiet <trouni@gmail.com>
2020-08-03 01:36:17 +0900

    fixed cookie id when logged out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,7 @@
 class SessionsController < Devise::SessionsController
 
   def create
-    cookies.signed[:user_id] = current_user.id
+    cookies.signed[:user_id] = current_user&.id
     super
   end
 
