b9ddc71c15f268263097d5850ef74f93782d9f7a
Trouni Tiet <trouni@gmail.com>
2020-08-02 22:05:07 +0900

    refactored action cable with user_id cookie

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,20 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    def disconnect
+      # ActionCable.server.remote_connections.where(current_user: current_user).disconnect
+      ScheduleUserJob.perform_now(job: 'disconnect', user_id: current_user.id, delay_in_seconds: 10)
+    end
+
+    protected
+
+    def find_verified_user
+      return User.find(cookies.signed[:user_id].to_i || 0)
+    end
   end
 end
