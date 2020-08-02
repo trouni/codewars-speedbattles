b2edb2a427417e7aca6ac19025039adcca23ed0d
Trouni Tiet <trouni@gmail.com>
2020-08-03 01:36:53 +0900

    broadcasting after invites etc

--- app/jobs/connect_user.rb
+++ app/jobs/connect_user.rb
@@ -4,13 +4,6 @@ class ConnectUser < RoomFlowJob
   def perform(user_id:, room_id: nil, force: false)
     @user.broadcast_settings
 
-    if @room
-      @room.broadcast_settings(private_to_user_id: @user.id)
-      @room.broadcast_users(private_to_user_id: @user.id) unless user_signed_in?
-      @room.broadcast_messages(private_to_user_id: @user.id)
-      @room.broadcast_active_battle(private_to_user_id: @user.id)
-    end
-
     if user_signed_in? && @room
       RoomUser.where(user: @user).where.not(room: @room).destroy_all
       RoomUser.find_or_create_by(room: @room, user: @user)
@@ -21,6 +14,13 @@ class ConnectUser < RoomFlowJob
         ScheduleRandomBattle.perform_now(room_id: @room.id, delay_in_seconds: 20) if @room.auto_schedule_new_battle?
       end
       @room.broadcast_users
+    end
+
+    if @room
+      @room.broadcast_settings(private_to_user_id: @user.id)
+      @room.broadcast_users(private_to_user_id: @user.id) unless user_signed_in?
+      @room.broadcast_messages(private_to_user_id: @user.id)
+      @room.broadcast_active_battle(private_to_user_id: @user.id)
     end
   end
 end
