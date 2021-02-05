d1e15d8c45d53cd13ba65911e76a978ab690613a
Trouni Tiet <trouni@gmail.com>
2021-02-05 13:22:05 +0900

    Added footer

--- app/javascript/views/home.vue
+++ app/javascript/views/home.vue
@@ -41,6 +41,9 @@
         </template>
       </widget>
     </div>
+    <footer>
+      Made by <a href="https://github.com/trouni"><i class="fab fa-github"></i> Trouni</a>
+    </footer>
   </div>
 </template>
 
@@ -75,6 +78,13 @@ export default {
       left: 50%;
       top: 50%;
       transform: translate(-50%, -50%);
+    }
+
+    footer {
+      position: absolute;
+      bottom: 1.5rem;
+      left: 50%;
+      transform: translateX(-50%);
     }
   }
 </style>
