aa51506d8f0ed69bb7aacddec8b4f8c1a00965a2
Trouni Tiet <trouni@gmail.com>
2021-02-05 15:13:27 +0900

    Displaying codewars username

--- app/javascript/components/settings/user_settings.vue
+++ app/javascript/components/settings/user_settings.vue
@@ -6,6 +6,18 @@
     </div>
     <div class="form-group mt-3 my-4" v-if="settings.user.signed_in">
       <div class="d-flex justify-content-between align-items-center">
+        <h5 class="m-0 no-wrap">Codewars username</h5>
+        <input
+          disabled
+          type="text"
+          :value="settings.user.username"
+          class="form-control ml-3"
+        />
+      </div>
+      <small>You can't change your Codewars username.</small>
+    </div>
+    <div class="form-group mt-3 my-4" v-if="settings.user.signed_in">
+      <div class="d-flex justify-content-between align-items-center">
         <h5 class="m-0 no-wrap">Display name</h5>
         <input
           @keydown.enter="updateSettings"
@@ -24,34 +36,56 @@
         <span class="d-flex justify-content-around flex-grow-1 ml-2">
           <std-button
             @click.native="$root.$emit('toggle-music')"
-            :class="{'toggled-off': !settings.user.music}"
-            :fa-icon="`fas ${settings.user.music ? 'fa-music' : 'fa-volume-mute'}`"
+            :class="{ 'toggled-off': !settings.user.music }"
+            :fa-icon="
+              `fas ${settings.user.music ? 'fa-music' : 'fa-volume-mute'}`
+            "
             :disabled="!settings.room.sound && !moderator"
-          >Music {{settings.user.music ? 'ON' : 'OFF'}}</std-button>
+            >Music {{ settings.user.music ? "ON" : "OFF" }}</std-button
+          >
           <std-button
             @click.native="$root.$emit('toggle-sound-fx')"
-            :class="[{'toggled-off': !settings.user.sfx}]"
+            :class="[{ 'toggled-off': !settings.user.sfx }]"
             :fa-icon="`fas ${settings.user.sfx ? 'fa-drum' : 'fa-volume-mute'}`"
-          >Sound FX {{settings.user.sfx ? 'ON' : 'OFF'}}</std-button>
+            >Sound FX {{ settings.user.sfx ? "ON" : "OFF" }}</std-button
+          >
           <std-button
             @click.native="$root.$emit('toggle-voice')"
-            :class="{'toggled-off': !settings.user.voice}"
-            :fa-icon="`fas ${settings.user.voice ? 'fa-robot' : 'fa-volume-mute'}`"
+            :class="{ 'toggled-off': !settings.user.voice }"
+            :fa-icon="
+              `fas ${settings.user.voice ? 'fa-robot' : 'fa-volume-mute'}`
+            "
             :disabled="!settings.room.sound && !moderator"
-          >Voice {{settings.user.voice ? 'ON' : 'OFF'}}</std-button>
+            >Voice {{ settings.user.voice ? "ON" : "OFF" }}</std-button
+          >
         </span>
       </div>
-      <small>Toggle music, sound effects and voice announcements.<span v-if="!settings.room.sound"><br><em>Sound settings have been partially disabled for this war room.</em></span></small>
+      <small
+        >Toggle music, sound effects and voice announcements.<span
+          v-if="!settings.room.sound"
+          ><br /><em
+            >Sound settings have been partially disabled for this war room.</em
+          ></span
+        ></small
+      >
     </div>
     <div class="form-group my-4" v-if="settings.user.signed_in">
       <div class="d-flex justify-content-between align-items-center">
         <h5 class="m-0">Preferred coding language</h5>
         <select v-model="hljsLang" class="flex-grow-1 ml-3">
           <option value></option>
-          <option v-for="(key, lang_name) in settings.room.codewars_langs" :value="lang_name" :key="key">{{ key }}</option>
+          <option
+            v-for="(key, lang_name) in settings.room.codewars_langs"
+            :value="lang_name"
+            :key="key"
+            >{{ key }}</option
+          >
         </select>
       </div>
-      <small>Select a preferred language for challenges and code blocks in the chat.</small>
+      <small
+        >Select a preferred language for challenges and code blocks in the
+        chat.</small
+      >
     </div>
     <div class="webhook-settings my-4" v-if="settings.user.signed_in">
       <webhook-instructions :settings="settings" />
@@ -62,17 +96,35 @@
         <std-button
           @click.native="$root.$emit('toggle-low-res')"
           :fa-icon="`fas ${lowRes ? 'fa-adjust' : 'fa-image'}`"
-        >{{ lowRes ? 'LOW RES' : 'STANDARD' }}</std-button>
+          >{{ lowRes ? "LOW RES" : "STANDARD" }}</std-button
+        >
       </div>
-      <small>Alternative theme that removes transparency and other effects for slower devices.</small>
+      <small
+        >Alternative theme that removes transparency and other effects for
+        slower devices.</small
+      >
     </div>
-    <div class="form-group my-4" v-if="settings.user.signed_in || settings.room.id">
+    <div
+      class="form-group my-4"
+      v-if="settings.user.signed_in || settings.room.id"
+    >
       <div class="d-flex justify-content-around">
-        <a href="/users/sign_out" data-method="delete" v-if="settings.user.signed_in">
-          <std-button @click.native="cancel" fa-icon="fas fa-sign-out-alt" small>Sign out</std-button>
+        <a
+          href="/users/sign_out"
+          data-method="delete"
+          v-if="settings.user.signed_in"
+        >
+          <std-button @click.native="cancel" fa-icon="fas fa-sign-out-alt" small
+            >Sign out</std-button
+          >
         </a>
         <a href="/rooms/" v-if="settings.room.id">
-          <std-button @click.native="cancel" fa-icon="fas fa-angle-double-left" small>Leave room</std-button>
+          <std-button
+            @click.native="cancel"
+            fa-icon="fas fa-angle-double-left"
+            small
+            >Leave room</std-button
+          >
         </a>
       </div>
     </div>
@@ -80,11 +132,11 @@
 </template>
 
 <script>
-import EventBus from '../../services/event_bus'
-import WebhookInstructions from './webhook_instructions'
+import EventBus from "../../services/event_bus";
+import WebhookInstructions from "./webhook_instructions";
 
 export default {
-  name: 'user-settings',
+  name: "user-settings",
   components: {
     WebhookInstructions
   },
@@ -93,14 +145,14 @@ export default {
     moderator: {
       type: Boolean,
       default: false
-    },
+    }
   },
   watch: {
     settings: {
       handler(newSettings) {
-        this.displayName = newSettings.user.name
-        this.hljsLang = newSettings.user.hljs_lang
-        this.lowRes = newSettings.user.low_res_theme
+        this.displayName = newSettings.user.name;
+        this.hljsLang = newSettings.user.hljs_lang;
+        this.lowRes = newSettings.user.low_res_theme;
       },
       deep: true
     }
@@ -109,8 +161,8 @@ export default {
     return {
       displayName: this.settings.user.name,
       hljsLang: this.settings.user.hljs_lang,
-      lowRes: this.settings.user.low_res_theme,
-    }
+      lowRes: this.settings.user.low_res_theme
+    };
   },
   computed: {
     updatedSettings() {
@@ -121,27 +173,32 @@ export default {
           voice: this.settings.user.voice,
           music: this.settings.user.music,
           hljs_lang: this.hljsLang,
-          low_res_theme: this.lowRes,
+          low_res_theme: this.lowRes
         }
-      }
-    },
+      };
+    }
   },
   mounted() {
-    EventBus.$on('submit', this.updateSettings)
-    EventBus.$on('cancel', this.cancel)
+    EventBus.$on("submit", this.updateSettings);
+    EventBus.$on("cancel", this.cancel);
   },
   methods: {
     cancel() {
-      this.$root.$emit('close-modal')
+      this.$root.$emit("close-modal");
     },
     updateSettings() {
-      this.$root.$emit('update-settings', this.updatedSettings)
-      this.$root.$emit('close-modal')
-    },
+      this.$root.$emit("update-settings", this.updatedSettings);
+      this.$root.$emit("close-modal");
+    }
   }
-}
+};
 </script>
 
-<style>
-
+<style scoped="true">
+input:disabled {
+  outline: none;
+  border: none;
+  opacity: 0.7;
+  user-select: none;
+}
 </style>
